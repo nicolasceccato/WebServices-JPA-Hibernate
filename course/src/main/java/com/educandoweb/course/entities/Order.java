4d5f392742fdc5cf381cfad21dcc757394b7e164
nicolasceccato <nicolasceccato@hotmail.com>
2023-06-14 08:45:37 -0300

    Criada classe OrderResource, OrderService e interface OrderRepository

--- course/src/main/java/com/educandoweb/course/entities/Order.java
+++ course/src/main/java/com/educandoweb/course/entities/Order.java
@@ -1,5 +1,6 @@
 package com.educandoweb.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -14,10 +15,11 @@ public class Order implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Instant moment;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
+    private Instant moment;
     @ManyToOne
-    @JoinColumn(name = "cliente_id")
+    @JoinColumn(name = "client_id")
     private User client;
 
     public Order() {
